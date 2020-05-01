9c37fde64a91362b0693033bd4ce49f277f37198
Thomas Eizinger <thomas@eizinger.io>
2020-05-01 15:06:18 +1000

    Implement single Receiver type for all states

--- src/puzzle_solver/receiver.rs
+++ src/puzzle_solver/receiver.rs
@@ -1,8 +1,63 @@
 use crate::bitcoin;
 use crate::puzzle_solver::Message4;
 use crate::secp256k1;
+use crate::UnexpectedMessage;
 use anyhow::Context;
 use std::convert::TryFrom;
+
+pub enum Receiver {
+    Receiver0(Receiver0),
+    Receiver1(Receiver1),
+}
+
+impl Receiver {
+    pub fn new(
+        X_r: secp256k1::PublicKey,
+        X_t: secp256k1::PublicKey,
+        unsigned_redeem_transaction: bitcoin::Transaction,
+        sig_redeem_t: secp256k1::EncryptedSignature,
+        sig_redeem_r: secp256k1::Signature,
+        beta: secp256k1::KeyPair,
+        redeem_tx_digest: bitcoin::SigHash,
+    ) -> Self {
+        let receiver0 = Receiver0::new(
+            X_r,
+            X_t,
+            unsigned_redeem_transaction,
+            sig_redeem_t,
+            sig_redeem_r,
+            beta,
+            redeem_tx_digest,
+        );
+
+        receiver0.into()
+    }
+
+    pub fn transition(self, message: In) -> anyhow::Result<Self> {
+        let receiver = match (self, message) {
+            (Receiver::Receiver0(inner), In::Message4(message)) => inner.receive(message)?.into(),
+            _ => anyhow::bail!(UnexpectedMessage),
+        };
+
+        Ok(receiver)
+    }
+}
+
+pub enum In {
+    Message4(Message4),
+}
+
+impl From<Receiver0> for Receiver {
+    fn from(receiver: Receiver0) -> Self {
+        Self::Receiver0(receiver)
+    }
+}
+
+impl From<Receiver1> for Receiver {
+    fn from(receiver: Receiver1) -> Self {
+        Self::Receiver1(receiver)
+    }
+}
 
 pub struct Receiver0 {
     X_r: secp256k1::PublicKey,
